9ddcc26394559db44372b00724c0a96c23197698
ZHOUYIJIEQM <husiluanxianguser@163.com>
2019-05-07 15:44:15 +0800

    fixed some

--- src/libs/util.js
+++ src/libs/util.js
@@ -21,7 +21,7 @@ Util.prevDay = function(timestamp=(Date.now())){
     ? '0' + (date.getMonth()+1)
     : date.getMonth()+1;
   const day = date.getDate()+1 < 10
-    ? '0' + (date.getDate()+1)
+    ? '0' + date.getDate()
     : date.getDate();
   return year + '' + month + '' + day;
 }
